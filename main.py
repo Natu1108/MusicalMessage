93d763ce983d2f011fa429b85c05a5ebd42b4ecf
Natu1108 <124746312+Natu1108@users.noreply.github.com>
2025-12-14 15:15:49 -0600

    Add MIDI file playback functionality

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 import tkinter as tk
-from tkinter import messagebox, filedialog
+from tkinter import messagebox, filedialog, dialog
 from mido import Message, MidiFile, MidiTrack
-import os
+import os, pygame, time
 
 # Mido variables
 mid = MidiFile()
 track = MidiTrack()
 mid.tracks.append(track)
+fp = ""
 
 # Converts the ASCII character to notes
 def encode():
+    global fp
     msg = entry.get()
 
     for letter in msg:
@@ -25,14 +27,16 @@ def encode():
 
 # Converts notes to ASCII characters
 def decode():
+    global fp
     decodedMsg = ""
-    for msg in MidiFile('output.mid').play():
+    for msg in MidiFile(fp).play():
         if msg.type in ('note_on', 'note_off'):
             decodedMsg += chr(msg.note)
     output.config(text=decodedMsg)
 
 # Choose the .mid file
 def chooseMid():
+    global fp
     f = filedialog.askopenfile(
         title="Select a file",
         filetypes=(
@@ -43,6 +47,21 @@ def chooseMid():
 
     if f:
         chooseBtn.config(text=os.path.basename(f.name))
+        fp = os.path.basename(f.name)
+
+# Plays the .mid file to show what the audio sounds like
+def playMid():
+    global fp
+    pygame.init()
+    pygame.mixer.music.load(fp)
+    pygame.mixer.music.play()
+
+    # Keep the script running while the music plays
+    while pygame.mixer.music.get_busy():
+        time.sleep(1)
+
+    pygame.mixer.music.stop()
+    pygame.mixer.quit()
 
 # Setup the window
 root = tk.Tk()
@@ -57,6 +76,7 @@ btn = tk.Button(root, text="Submit", command=encode)
 btnDecode = tk.Button(root, text="Decode", command=decode)
 chooseBtn = tk.Button(root, text="Choose a file", command=chooseMid)
 labelDecode = tk.Label(root, text="Decode a message: ")
+btnPlay = tk.Button(root, text="Play the audio", command=playMid)
 output = tk.Label(root, text="")
 
 # Place widgets on screen
@@ -66,7 +86,8 @@ btn.grid(row=0, column=3)
 labelDecode.grid(row=1,column=0)
 chooseBtn.grid(row=1, column=1)
 btnDecode.grid(row=1, column=2)
-output.grid(row=2)
+btnPlay.grid(row=2)
+output.grid(row=3, columnspan=3)
 
 # Run the app
 root.mainloop()
